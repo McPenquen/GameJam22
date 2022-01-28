e2bd598a93f3e249e6c1d1f0992b101a54b81d42
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-01-28 22:33:57 +0000

    Added comment telling me what to do

---
+++ Assets/Scripts/p2Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2Movement : MonoBehaviour
+{
+    //This script is for the movement of the 2nd player. detached from cam. no collision. arrow keys from movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

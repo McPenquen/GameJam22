aa7f2316d3b8b3a605937147d664151b8a001d46
McPenquen <majda.calk@outlook.com>
2022-01-29 20:38:49 +0000

    Finish Menu

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
     public void StartGame()
     {
         // Load the scene with id 1
         SceneManager.LoadScene(1);
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }

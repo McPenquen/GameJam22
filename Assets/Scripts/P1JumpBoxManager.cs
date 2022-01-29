0ef9da3406c0dff0d2a57cddc3be34165fc940aa
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 14:37:12 +0200

    Update P1JumpBoxManager.cs

--- Assets/Scripts/P1JumpBoxManager.cs
+++ Assets/Scripts/P1JumpBoxManager.cs
@@ -1,14 +1,22 @@
-class P1JumpBoxManager {
-	[HideInInspector] public boolean CanJump { get { return hitCount == 0; } };
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class P1JumpBoxManager : MonoBehaviour
+{
+	[HideInInspector] public bool CanJump { get { return hitCount == 0; } };
 	int hitCount = 0;
-	
-	void OnCollisionEnter() {
+
+	void OnCollisionEnter()
+	{
 		hitCount++;
 	}
-	
-	void OnCollisionLeave() {
+
+	void OnCollisionLeave()
+	{
 		hitCount--;
-		if (hitCount < 0) {
+		if (hitCount < 0)
+		{
 			Debug.Log("Player 1 jump box collision count is " + hitCount + ".");
 		}
 	}

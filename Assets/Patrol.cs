d5478d0cd5e48c12a58cf35aa08f884aeeaf4230
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 11:25:41 +0000

    Patrol Script Can Wait
    The patrolling script now has a wait toggle that makes the platform wait a bit before moving on.

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -6,8 +6,13 @@ public class Patrol : MonoBehaviour
 {
     public Transform[] waypoints;
     public int speed;
+    public bool waitToggle = false;
 
     private int waypointIndex = 0;
+
+    private float waitTime = 1f; // in seconds
+    private float waitCounter = 0f;
+    private bool waiting = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +24,24 @@ public class Patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (waiting)
+        {
+            waitCounter += Time.deltaTime;
+            if (waitCounter < waitTime)
+                return;
+            waiting = false;
+        }
+
         Transform wp = waypoints[waypointIndex];
         if (Vector3.Distance(transform.position, wp.position) < 0.01f)
         {
+            if (waitToggle == true)
+            {
+                transform.position = wp.position;
+                waitCounter = 0f;
+                waiting = true;
+            }
+
             waypointIndex = (waypointIndex + 1) % waypoints.Length;
         }
         else

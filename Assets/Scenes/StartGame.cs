f1bd2b7eef2cc7f8b531d6d99b49f7fdedec51ea
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 13:00:52 +0000

    Start Game Button
    Prints "Start Game" to the console when Enter is clicked no matter where the cursor is.

---
+++ Assets/Scenes/StartGame.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Print();
+        }
+    }
+
+    public void Print()
+    {
+        print("Start Game");
+    }
+}

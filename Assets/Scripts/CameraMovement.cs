f2a964d071668fad69df60bb5a9d2e5c5d6daf5b
Camie Callaghan <camiecallaghan@gmail.com>
2022-01-29 21:25:46 +0000

    Update CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,7 +10,8 @@ class CameraMovement : MonoBehaviour
     public float p2Strength;
 	public float initialDelay = 5.0f;
 	public float camSlideSpeed = 0.1f;
-	
+	public float lowerLimit = float.NegativeInfinity;
+	public float upperLimit = float.PositiveInfinity;
 	
     void Awake()
     {
@@ -32,5 +33,6 @@ class CameraMovement : MonoBehaviour
 		}
 		
         Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(0.0f, Mathf.Lerp(p1Transform.position.y, p2Transform.position.y, p2Strength), Camera.main.transform.position.z), camSlideSpeed);
+		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Mathf.Clamp(Camera.main.transform.position.y, lowerLimit, upperLimit), Camera.main.transform.position.z);
     }
 }

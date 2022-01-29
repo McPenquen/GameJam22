92c1b1a0e8ad5a301f2f8b6cd9ac9cfbc0ac7f0f
Camie Callaghan <camiecallaghan@gmail.com>
2022-01-29 16:00:59 +0000

    .

--- Assets/Scripts/P1JumpBoxManager.cs
+++ Assets/Scripts/P1JumpBoxManager.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 class P1JumpBoxManager : MonoBehaviour
 {
-	[HideInInspector] public bool CanJump { get { return hitCount == 0; } }
-	int hitCount = 0;
+	[HideInInspector] public bool CanJump { get { return true /*hitCount > 0*/; } }
+	public int hitCount = 0;
 
-	void OnCollisionEnter()
+	void OnCollisionEnter2D(Collision2D collider)
 	{
-		hitCount++;
+		Debug.Log("Yeah I do exist");
+		if (collider.otherCollider.gameObject.tag == "Ground")
+			hitCount++;
 	}
-
-	void OnCollisionLeave()
+	
+	void OnCollisionLeave2D(Collision2D collider)
 	{
-		hitCount--;
-		if (hitCount < 0)
-		{
-			Debug.Log("Player 1 jump box collision count is " + hitCount + ".");
+		if (collider.otherCollider.gameObject.tag == "Ground") {
+			hitCount--;
+			if (hitCount < 0)
+			{
+				Debug.Log("Player 1 jump box collision count is " + hitCount + ".");
+			}
 		}
 	}
 }

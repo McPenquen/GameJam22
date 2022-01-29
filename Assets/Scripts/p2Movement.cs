61bf75f677135f244343bc02f4e951a3151d49b4
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-01-29 10:35:05 +0000

    Basic player 2 movement
    Basic player 2 movement implemented. Needs limitations for screen

--- Assets/Scripts/p2Movement.cs
+++ Assets/Scripts/p2Movement.cs
@@ -8,16 +8,25 @@ public class p2Movement : MonoBehaviour
     //Edit -> Project Settings -> Input Manager
 
     // Start is called before the first frame update
+
+    public float movementSpeed = 5.0f; 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Remember to change  the inputs in unity
-        float horizontalInput = Input.GetAxis("Horizontal2");
-        float verticalInput = Input.GetAxis("Vertical2");
+        //float horizontalInput = Input.GetAxis("Horizontal2");
+        //float verticalInput = Input.GetAxis("Vertical2");
+
+        Vector2 inputVel = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2")); 
+
+        inputVel = inputVel * movementSpeed * Time.deltaTime;
+
+        transform.Translate(inputVel); 
     }
 }

c70c40c58c246059c109001abbf3ca84c2b7b32f
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-01-29 11:48:34 +0000

    Update p2Movement.cs
    Player movements and boundaries working. Could use improvement

--- Assets/Scripts/p2Movement.cs
+++ Assets/Scripts/p2Movement.cs
@@ -10,8 +10,13 @@ public class p2Movement : MonoBehaviour
     // Start is called before the first frame update
 
     public float movementSpeed = 5.0f; 
+    float halfScreenHeight = 4.5f;
+    float halfScreenWidth = 10.0f; 
+    //private Vector2 screenBounds = new Vector2();
+
     void Start()
     {
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -24,9 +29,51 @@ public class p2Movement : MonoBehaviour
         //float verticalInput = Input.GetAxis("Vertical2");
 
         Vector2 inputVel = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2")); 
+        //Vector2 inputVel = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2")); 
+
+        Vector2 inputVelTemp = new Vector2(); 
+         
+        inputVelTemp = inputVel; 
 
         inputVel = inputVel * movementSpeed * Time.deltaTime;
 
+        if(transform.position.x >= halfScreenWidth)
+        {
+            //transform.position.x -= 0.5f;
+            inputVel.x = 0.0f; 
+            inputVel.x -= 0.5f;            
+        }
+
+        if(transform.position.x <= -halfScreenWidth)
+        {
+            //transform.position.x += 0.5f;
+            inputVel.x = 0.0f;
+            inputVel.x += 0.5f;            
+        }
+
+        if(transform.position.y >= halfScreenHeight)
+        {
+            //transform.position.y -= 0.5f;
+            inputVel.y = 0.0f;
+            inputVel.y -= 0.5f;            
+        }
+
+        if(transform.position.y <= -halfScreenHeight)
+        {
+            //transform.position.y += 0.5f;
+            inputVel.y = 0.0f;
+            inputVel.y += 0.5f;            
+        }
+
         transform.Translate(inputVel); 
+        
+        /*
+        Vector3 viewPos = new Vector3();
+        viewPos = transform.position; 
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x * -1);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y * -1);
+
+        transform.position = viewPos; 
+        */
     }
 }

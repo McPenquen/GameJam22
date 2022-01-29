01d91349d78951769d94d399cff8ea1dc8c8591a
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 14:38:21 +0200

    Update P1Movement.cs

--- Assets/Scripts/P1Movement.cs
+++ Assets/Scripts/P1Movement.cs
@@ -1,41 +1,52 @@
-class P1Movement {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class P1Movement : MonoBehaviour
+{
 	// Components
 	BoxCollider2D jumpBox;
 	P1JumpBoxManager jbm;
 	Rigidbody2D rb;
-	
+
 	// Left Right Movement Variables
 	public float playerMoveSpeed;
-	
+
 	// Jump Variables
 	public float jumpForce;
 	public float fastFallMod = 1.5f;
-	
+
 	// Set up Internal Variables
-	void Awake() {
+	void Awake()
+	{
 		rb = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Set up External Variables
-	void Start() {
-		jumpBox = GameObject.Find("p1JumpBox");
+	void Start()
+	{
+		jumpBox = GameObject.Find("p1JumpBox").GetComponent<BoxCollider2D>();
 		jbm = jumpBox.GetComponent<P1JumpBoxManager>();
 	}
-	
-	void Update() {
+
+	void Update()
+	{
 		if (Input.GetKey(KeyCode.A))
 			rb.velocity = new Vector2(-playerMoveSpeed, rb.velocity.y);
 		if (Input.GetKey(KeyCode.D))
 			rb.velocity = new Vector2(playerMoveSpeed, rb.velocity.y);
-		
-		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.W)) {
-			if (jbm.CanJump) {
+
+		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.W))
+		{
+			if (jbm.CanJump)
+			{
 				rb.velocity += new Vector2(0.0f, jumpForce);
 			}
 		}
-		
-		if (rb.velocity.y < 0.0f) {
-			rb.velocity += new Vector2(0.0f, Physics2D.gravity * fastFallMod);
+
+		if (rb.velocity.y < 0.0f)
+		{
+			rb.velocity += new Vector2(0.0f, Physics2D.gravity.y * fastFallMod);
 		}
 	}
 }

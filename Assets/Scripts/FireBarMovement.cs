9927c3163cd157f88dc7380c93ce2881b1c26de7
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 14:36:39 +0200

    Update FireBarMovement.cs

--- Assets/Scripts/FireBarMovement.cs
+++ Assets/Scripts/FireBarMovement.cs
@@ -1,7 +1,13 @@
-class FireBarMovement {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class FireBarMovement : MonoBehaviour
+{
 	public float speed;
-	
-	void Update() {
+
+	void Update()
+	{
 		transform.position += new Vector3(0.0f, speed, 0.0f);
 	}
 }

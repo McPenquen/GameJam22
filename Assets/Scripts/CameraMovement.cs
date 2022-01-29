615633cc22106e25f6f5c363692c2e12c3cad793
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-29 14:36:04 +0200

    Update CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,20 +1,28 @@
-class CameraMovement {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class CameraMovement : MonoBehaviour
+{
 	Transform p1Transform;
-	Transform p2Transform;
-	[Range(0.0f,1.0f)]
-	public float p2Strength;
-	
-	void Awake() {
-	
-	}
-	
-	void Start() {
-		// @TODO: Replace temp call names
-		p1Transform = GameObject.Find("Player1");
-		p2Transform = GameObject.Find("Player2");	
-	}
-	
-	void Update() {	
-		Camera.Main.Transform.position = new Vecctor3(0.0f, Mathf.Lerp(p1Transform.position.y, p2Transform.position.y, p2Strength, Camera.Main.Transform.position.z);
-	}
+    Transform p2Transform;
+    [Range(0.0f, 1.0f)]
+    public float p2Strength;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        // @TODO: Replace temp call names
+        p1Transform = GameObject.Find("Player1").transform;
+        p2Transform = GameObject.Find("Player2").transform;
+    }
+
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(0.0f, Mathf.Lerp(p1Transform.position.y, p2Transform.position.y, p2Strength), Camera.main.transform.position.z);
+    }
 }

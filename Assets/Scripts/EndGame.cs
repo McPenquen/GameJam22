0685bb510ea6225ca37bb7fb5d64b3fd269cb6d7
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2022-01-30 10:43:53 +0000

    Implemented a Finish Line Tilemap

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour
+{
+    public float timeUntilWin = 1f;
+    private float currentTime = 0;
+    private bool updateTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (updateTime)
+        {
+            currentTime += Time.deltaTime;
+        }
+        if (currentTime >= timeUntilWin)
+        {
+            updateTime = false;
+            currentTime = 0;
+            SceneManager.LoadScene(sceneBuildIndex: 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        updateTime = true;
+    }
+}
